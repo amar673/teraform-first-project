1a7e19c6c464894a4463cd2939df7e49338ce614
amar673 <amarnathsrivastav76@gmail.com>
2025-07-16 10:07:12 +0000

    third

--- resource.tf
+++ resource.tf
@@ -1,19 +1,21 @@
- 
-resource "azurerm_resource_group" "storeterra" {
-    name     = "example-resources"
-    location = "East US"
+provider "azurerm" {
+  features {}
 }
  
-resource "azurerm_storage_account" "saterra" {
-    name                     = "terrastorage673"
-    resource_group_name      = azurerm_resource_group.storeterra.name
-    location                 = azurerm_resource_group.storeterra.location
-    account_tier             = "Standard"
-    account_replication_type = "LRS"
+resource "azurerm_resource_group" "terra" {
+  name     = "terra"
+  location = "East US"  # Change to correct location if needed
 }
  
-resource "azurerm_storage_container" "sacontainer" {
-    name                  = "terra-container673"
-    storage_account_name  = azurerm_storage_account.saterra.name
-    container_access_type = "private"
+resource "random_integer" "rand" {
+  min = 1000
+  max = 9999
+}
+ 
+resource "azurerm_storage_account" "mystorage" {
+  name                     = "mystoracct${random_integer.rand.result}"
+  resource_group_name      = azurerm_resource_group.terra.name
+  location                 = azurerm_resource_group.terra.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
 }
